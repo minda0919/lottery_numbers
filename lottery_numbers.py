5fb567655766ea4ed10c7d28bb9ca63918a87d1c
minda0919 <minda.shi@mail.utoronto.ca>
2023-11-15 03:02:20 -0500

    Fixed gen_lotto_nums, program currently usable

--- lottery_numbers.py
+++ lottery_numbers.py
@@ -16,12 +16,12 @@ Current functionality:
 
 Possible future functionality:
     - Predict next winning numbers
+    - Allowing inputs with blank spaces (more flexibility with inputs)
 """
+# ADD COMMENTS
+# UPDATE README
 
 import random as rd
-
-def test(x):
-    return x + 1
 
 
 def gen_lotto_nums(num):
@@ -38,63 +38,67 @@ def gen_lotto_nums(num):
         num_pool = list(range(1, 50))
         
         if last_draw != []:
-            for index in num_pool:
-                for index in last_draw:
-                    num_pool.remove(index)
+            for number in last_draw:
+                num_pool.remove(number)
         
         current_draw = rd.sample(num_pool, 6)
         last_draw = current_draw
         all_draws.append(current_draw)
+        current_num = current_num + 1
         
     return all_draws
 
 
 def print_draws(all_draws, num_draws):
+    print('')
+    print('')
     if num_draws == 1:
-        print("Here are the numbers generated from your draw:")
+        print('Here are the numbers generated from your draw:')
     else:
-        print("Here are the numbers generated from your", num_draws, "draws:")
+        print('Here are the numbers generated from your', num_draws, 'draws:')
 
+    print('')
     for draw in all_draws:
-        print("[", draw, "]")
-        print("")
+        print('Draw', all_draws.index(draw) + 1)
+        print(draw)
+        print('')
 
 
 while True:
     try:
-        num_draws = int(input("Please input the number of draws to do: "))
+        num_draws = int(input('Please input the number of draws to do: '))
 
         if num_draws < 0:
-            print("Nice try, but you can't break my program that easily!")
-            print("")
+            print('Nice try, but you can\'t break my program that easily!')
+            print('')
             continue
         elif num_draws == 0:
-            print("Good for you!")
-            print("Gambling addictions are serious. I read once that you're more likely to die on the way to buying a lottery ticket than you are to win the lottery.")
-            print("")
-            print("Thanks for trying out my program!")
-            print("Terminating...")
+            print('')
+            print('Good for you!')
+            print('Gambling addictions are serious. I read once that you\'re more likely to die on the way to buying a lottery ticket than you are to win the lottery.')
+            print('')
+            print('Thanks for trying out my program!')
+            print('Terminating...')
             break
         else:
-            draws = test(num_draws)
             print_draws(gen_lotto_nums(num_draws), num_draws)
             
             while True:
-                print("")
-                cont = str(input("Would you like to draw more numbers? [y/n] "))
+                print('')
+                cont = str(input('Would you like to draw more numbers? [y/n] '))
                 
                 if cont == 'n' or cont == 'N':
-                    print("")
-                    print("Thanks for trying out my program!")
-                    print("Terminating...")
+                    print('')
+                    print('Thanks for trying out my program!')
+                    print('Terminating...')
                     break
                 elif cont == 'y' or cont == 'Y':
                     break
                 else:
-                    print("Please type y or n.")
+                    print('Please type y or n as indicated.')
     except:
-        print("Please input an integer.")
-        print("")
+        print('Please input an integer.')
+        print('')
         continue
 
     if cont == 'y' or cont == 'Y':
